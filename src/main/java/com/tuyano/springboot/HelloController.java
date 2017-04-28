4e54d0c9f3f0a512e47f179f7531b0270d89581f
佐々木 伸也 <s-shinya@sasakinoMacBook-Pro.local>
2017-04-28 22:35:36 +0900

    p120 @PathValiable

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,13 +1,17 @@
 package com.tuyano.springboot;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
-	@RequestMapping("/")
-	public String index() {
-		return "Hello Spring-Boot World!";
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+		int result = 0;
+		for(int i = 0; i <= num; i++) 
+			result += i;
+		return "total: " + result;
 	}
 }
