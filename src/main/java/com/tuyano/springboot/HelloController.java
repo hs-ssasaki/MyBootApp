441c5de5959c536657775fed9c843323c17b681c
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-04-29 19:01:23 +0900

    p123 @RestController, return object

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -6,12 +6,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	String[] names = {"tuyano", "hanako", "taro",
+					  "sachiko", "ichiro"};
+	String[] mails = {"syoda@tuyano.com", "hanako@flower", 
+					  "taro@yamada", "sachiko@happy", "ichiro@baseball"};
 	
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num) {
-		int result = 0;
-		for(int i = 0; i <= num; i++) 
-			result += i;
-		return "total: " + result;
+	@RequestMapping("/{id}")
+	public DataObject index(@PathVariable int id) {	
+		return new DataObject(id, names[id], mails[id]);
 	}
+}
+
+class DataObject {
+	private int id;
+	private String name;
+	private String value;
+	
+	public DataObject(int id, String name, String value) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.value = value;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}	
 }
