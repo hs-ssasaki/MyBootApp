985d45f93dad83064b6452d39147a57e98882726
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-04-30 17:27:15 +0900

    p137 WebForm and @RequestParam

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,24 +1,31 @@
 package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-	
-	@RequestMapping("/{num}")
-	// 戻り値を ModelAndView クラスに変更、引数に ModelAndView クラス追加
-	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
-		int result = 0;
-		for (int i = 1; i <= num; i++) {
-			result += i;
-		}
-		// ModelAndView#addObject で値を渡す
-		mav.addObject("msg", "total: " + result);
-		// ModelAndView#setViewName でテンプレート名を渡す
+
+	// @RequestMapping は、
+	// value でパス指定、method でメソッド指定
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("msg", "お名前を入力してください");
 		return mav;
 	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	// @PathValiable と同様に、フォームからパラメータを受け取るには、
+	// @RequestParam(フォームパラメータ名) を引数につける
+	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "こんにちは" + str + "さん");
+		mav.addObject("value", str);
+		return mav;
+	}
+
 }
