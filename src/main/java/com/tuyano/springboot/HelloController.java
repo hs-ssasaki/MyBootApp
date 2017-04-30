9fb4f602a813f2eb5f1b7049c83a6ab3e052675c
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-04-30 19:08:49 +0900

    p140 @RequestParam and WebForm2

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -9,23 +9,38 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
-	// @RequestMapping は、
-	// value でパス指定、method でメソッド指定
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg", "お名前を入力してください");
+		mav.addObject("msg", "フォームを送信してください");
 		return mav;
 	}
 	
 	@RequestMapping(value="/", method=RequestMethod.POST)
-	// @PathValiable と同様に、フォームからパラメータを受け取るには、
-	// @RequestParam(フォームパラメータ名) を引数につける
-	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav) {
+	// @RequestParam(value=フォーム名, required=false) で非必須パラメータの指定ができる 
+	public ModelAndView send(
+			@RequestParam(value="check1", required=false)boolean check1,
+			@RequestParam(value="radio1", required=false)String radio1,
+			@RequestParam(value="select1", required=false)String select1,
+			@RequestParam(value="select2", required=false)String[] select2,
+			ModelAndView mav) {
+		String res = "";
+		try {
+			res = "check1: " + check1 +
+					" radio1: " + radio1 +
+					" select1: " + select1 +
+					" select2:";
+		} catch (NullPointerException e) {}
+		try {
+			res += select2[0];
+			for(int i = 1; i < select2.length; i++) {
+				res += ", " + select2[i];
+			}
+		}catch (NullPointerException e) {
+			res += "null";
+		}
 		mav.setViewName("index");
-		mav.addObject("msg", "こんにちは" + str + "さん");
-		mav.addObject("value", str);
+		mav.addObject("msg", res);
 		return mav;
 	}
-
 }
