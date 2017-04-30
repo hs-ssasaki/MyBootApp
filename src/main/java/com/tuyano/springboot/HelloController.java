2476df8a9ba56676cd3d42b4bbc0c326df68ba9b
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-04-30 12:42:47 +0900

    p133 Themeleaf and Model

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,15 +1,22 @@
 package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 	
-	@RequestMapping("/")
-	public String index() {
-		// pom.xmlで spring-boot-starter-thymeleaf を設定しているため、
-		// 名前で resource/templates 以下のテンプレートが自動ロードされる
+	@RequestMapping("/{num}")
+	// 引数に Model クラス追加
+	public String index(@PathVariable int num, Model model) {
+		int result = 0;
+		for (int i = 1; i <= num; i++) {
+			result += i;
+		}
+		// Model#addAttribute で値を渡す
+		model.addAttribute("msg", "total: " + result);
 		return "index";
 	}
 }
