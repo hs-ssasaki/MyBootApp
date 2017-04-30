37adca41553a0d35273b2c2899cd2fcd164a4261
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-04-30 12:55:08 +0900

    p134 ModelAndView

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,22 +1,24 @@
 package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 	
 	@RequestMapping("/{num}")
-	// 引数に Model クラス追加
-	public String index(@PathVariable int num, Model model) {
+	// 戻り値を ModelAndView クラスに変更、引数に ModelAndView クラス追加
+	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
 		int result = 0;
 		for (int i = 1; i <= num; i++) {
 			result += i;
 		}
-		// Model#addAttribute で値を渡す
-		model.addAttribute("msg", "total: " + result);
-		return "index";
+		// ModelAndView#addObject で値を渡す
+		mav.addObject("msg", "total: " + result);
+		// ModelAndView#setViewName でテンプレート名を渡す
+		mav.setViewName("index");
+		return mav;
 	}
 }
