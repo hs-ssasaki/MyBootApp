3f87d3a1dde6cf3416b6f85f5d89c2e604137e2e
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-04-30 12:12:00 +0900

    p131 @Controller, Thymeleaf

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,55 +1,15 @@
 package com.tuyano.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-	String[] names = {"tuyano", "hanako", "taro",
-					  "sachiko", "ichiro"};
-	String[] mails = {"syoda@tuyano.com", "hanako@flower", 
-					  "taro@yamada", "sachiko@happy", "ichiro@baseball"};
 	
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id) {	
-		return new DataObject(id, names[id], mails[id]);
+	@RequestMapping("/")
+	public String index() {
+		// pom.xmlで spring-boot-starter-thymeleaf を設定しているため、
+		// 名前で resource/templates 以下のテンプレートが自動ロードされる
+		return "index";
 	}
-}
-
-class DataObject {
-	private int id;
-	private String name;
-	private String value;
-	
-	public DataObject(int id, String name, String value) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}	
 }
