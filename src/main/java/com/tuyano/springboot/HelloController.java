0c7bb35b136784d2e1e68a182c91ecfe2c7b44bb
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-05-04 10:44:59 +0900

    p210 @Entity and JPARepository

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,9 +1,9 @@
 package com.tuyano.springboot;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,30 +15,34 @@ public class HelloController {
 	@Autowired
 	MyDataRepository myDataRepository;
 	
-	/* JPAのエンティティクラスのインスタンスを自動生成し、
-	 * Viewに渡すモデルにaddAttributeするアノテーション @ModelAttribute が用意されている、
-	 * @ModelAttribute("formModel") MyData mydata は、
-	 * メソッドコール時に、自動で MyData エンティティクラスのインスタンス mydata を自動生成し（つまり、DBエントリを読み込み）、
-	 * さらに、model.addAttribute("formModel", mydata) する */
 	@RequestMapping(value = "/", method=RequestMethod.GET)
-	public ModelAndView index(
-			@ModelAttribute("formModel") MyData mydata,
-			ModelAndView mav) {
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
 		Iterable<MyData> list = myDataRepository.findAll();
 		mav.addObject("msg", "this is sample content.");
 		mav.addObject("datalist", list);
-		
 		return mav;
 	}
 	
-	/* @Transactional(readOnly = fase) でデータの更新もできるトランザクションを設定 */
-	@RequestMapping(value="/", method=RequestMethod.POST)
-	@Transactional(readOnly = false)
-	public ModelAndView form(
-			@ModelAttribute("formModel") MyData mydata,
-			ModelAndView mav) {
-		myDataRepository.saveAndFlush(mydata);
-		return new ModelAndView("redirect:/");
+	@PostConstruct
+	public void init() {
+		MyData data1 = new MyData();
+		data1.setName("Tuyano");
+		data1.setAge(23);
+		data1.setMail("aaa@xxdd");
+		data1.setMemo("this is data1");
+		MyData data2 = new MyData();
+		data2.setName("Muyano");
+		data2.setAge(42);
+		data2.setMail("afe@xxdd");
+		data2.setMemo("this is data2");
+		MyData data3 = new MyData();
+		data3.setName("Yuyano");
+		data3.setAge(45);
+		data3.setMail("dijfe@xxdd");
+		data3.setMemo("this is data3");
+		myDataRepository.saveAndFlush(data1);
+		myDataRepository.saveAndFlush(data2);
+		myDataRepository.saveAndFlush(data3);
 	}
 }
