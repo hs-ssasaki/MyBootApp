f14a60765a83ed67e5367e8f680fe4d2887d31b2
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-05-21 12:17:32 +0900

    p333 @RestController, and JQuery client

---
+++ src/main/java/com/tuyano/springboot/MyDataRestController.java
@@ -0,0 +1,30 @@
+package com.tuyano.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * RESTサービスの作り方。
+ * サーバでは、@RestController で、Javaオブジェクトを返却するだけ（自動でJSONに変換して送信される）
+ * クライアントでは、JQuery で、受信結果をJavascriptオブジェクトとして処理するだけ（自動でJSONをJavascriptオブジェクトとして取り込まれる）
+ */
+@RestController
+public class MyDataRestController {
+	@Autowired
+	MyDataService myDataService;
+	
+	@RequestMapping(value = "/rest")
+	public List<MyData> restAll() {
+		return (List<MyData>)myDataService.getAll();
+	}
+
+	@RequestMapping(value="/rest/{num}")
+	public MyData rest(@PathVariable int num) {
+		return (MyData)myDataService.get(num);
+	}
+	
+}
